280932253aede88969e6565d19b2caf23eb40b40
Julian <julianser@hotmail.com.ar>
2023-03-09 18:03:28 -0300

    Se realizo el ejercicio2funciones

---
+++ js/ejercicio2funciones.js
@@ -0,0 +1,27 @@
+// 2- Definir una función que muestre información sobre una cadena de texto que se le pasa como argumento. A partir de la cadena que se le pasa, la función determina si esa cadena está formada sólo por mayúsculas, sólo por minúsculas o por una mezcla de ambas.
+
+let cadena = prompt("Ingrese una cadena de texto");
+
+maymin(cadena);
+
+function maymin(texto)
+{
+    if(texto)
+    {
+    if(cadena === cadena.toUpperCase())
+    {
+        document.write(`El texto ingresado (${texto}) esta conformado solo por mayúsculas`)
+    }
+    else if(cadena === cadena.toLowerCase())
+    {
+        document.write(`El texto ingresado (${texto}) esta conformado solo por minúsculas`)
+    }
+    else
+    {
+        document.write(`El texto ingresado (${texto}) esta conformado por mayúsculas y minúsculas`)
+    }
+    }
+    else{
+        document.write("Usted no ingreso una cadena")
+    }
+}
