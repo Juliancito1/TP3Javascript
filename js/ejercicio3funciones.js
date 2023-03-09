8af8321798bc0837e3a414fce3c74c67ade6c3c9
Julian <julianser@hotmail.com.ar>
2023-03-09 18:45:02 -0300

    Se realizo la validacion de que sea numero lo que se ingrese

--- js/ejercicio3funciones.js
+++ js/ejercicio3funciones.js
@@ -9,6 +9,12 @@ perimetrorectangulo(lado1,lado2);
 
 function perimetrorectangulo(num1,num2)
 {
+    if(num1 && num2 && !isNaN(num1,num2))
+    {
     let suma = 2*(num1+num2);
-    return document.write(`El perimetro del rectangulo es: ${suma}`)
+    return document.write(`El perimetro del rectangulo es: ${suma}`);
+    }
+    else{
+        document.write("Usted ingreso un solo numero o ningun numero");
+    }
 }
