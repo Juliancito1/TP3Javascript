7eed86a7284b0cbe622bd98ec7d87d7ed95f89ef
Julian <julianser@hotmail.com.ar>
2023-03-09 17:44:58 -0300

    Se realizo el ejercicio1funciones

---
+++ js/ejercicio1funciones.js
@@ -0,0 +1,18 @@
+//1- Escribir el código de una función a la que se pasa como parámetro un número entero y devuelve como resultado una cadena de texto que indica si el número es par o impar. Mostrar por pantalla el resultado devuelto por la función.
+
+
+let x = 2;
+paridadnum(x);
+
+
+function paridadnum(numero)
+{
+    if(numero % 2 === 0)
+    {
+        document.write(`El numero ${numero} es Par`)
+    }
+    else
+    {
+        document.write(`El numero ${numero} es Impar`)
+    }
+}
