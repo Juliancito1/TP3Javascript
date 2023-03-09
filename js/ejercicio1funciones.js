dad3dc5804c20d79cff6cbf7b8af3e8b7ac33fcf
Julian <julianser@hotmail.com.ar>
2023-03-09 18:31:35 -0300

    Se realizo la validacion de que lo ingresado sea un numero

--- js/ejercicio1funciones.js
+++ js/ejercicio1funciones.js
@@ -1,12 +1,14 @@
 //1- Escribir el código de una función a la que se pasa como parámetro un número entero y devuelve como resultado una cadena de texto que indica si el número es par o impar. Mostrar por pantalla el resultado devuelto por la función.
 
-
 let x = 2;
+   
 paridadnum(x);
 
 
 function paridadnum(numero)
 {
+    if(numero && !isNaN(numero))
+    {
     if(numero % 2 === 0)
     {
         document.write(`El numero ${numero} es Par`)
@@ -14,5 +16,9 @@ function paridadnum(numero)
     else
     {
         document.write(`El numero ${numero} es Impar`)
+    }
+    }
+    else{
+        document.write("Usted no ingreso un numero");
     }
 }
